ff145fcdc6308d7ec1880be585c14865ede25154
Chauntae Johnson <jchauntae@gmail.com>
2023-09-17 16:30:21 -0400

    Second Commit

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+// const express = require("express");
+// const app = express();
+
+// const port = 3000;
+
+// app.get("/greeting/:name", (req, res) => {
+//   const name = req.params.name;
+//   res.send(`Wow! Hello there, ${name}!`);
+// });
+
+// app.listen(port,() => {
+//     console.log('listening on port' , port);
+// });
+
 const express = require("express");
 const app = express();
 
 const port = 3000;
 
-app.get("/greeting/:name", (req, res) => {
-  const name = req.params.name;
-  res.send(`Wow! Hello there, ${name}!`);
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const total = parseFloat(req.params.total);
+  const tipPercentage = parseFloat(req.params.tipPercentage);
+
+  const tipAmount = (total * tipPercentage) / 100;
+  res.send(tipAmount.toString());
 });
 
-app.listen(port,() => {
-    console.log('listening on port' , port);
+app.listen(port, () => {
+  console.log("listening on port", port);
 });
